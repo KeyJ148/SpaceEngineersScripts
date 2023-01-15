05879cd195a7f95ce69d39d52b30aadad8cc4cd2
KeyJ148 <kejik148@gmail.com>
2023-01-15 12:25:28 +0400

    Create cergo minitors

---
+++ SharedScripts/Scripts/CargoItemsMonitor.cs
@@ -0,0 +1,72 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public class CargoItemsMonitor : IMonitor
+        {
+            private readonly Display display;
+            private readonly List<IMyEntity> containers;
+            private readonly Dictionary<Item, long> itemToMaxCount;
+            private readonly int maxNameLength;
+            private readonly string headerText;
+            private readonly ProgressbarSettings progressbarSettings;
+
+            public CargoItemsMonitor(Display display, List<IMyEntity> containers, Dictionary<Item, long> itemToMaxCount, string headerText,
+                ProgressbarSettings progressbarSettings)
+            {
+                this.display = display;
+                this.containers = containers;
+                this.headerText = headerText;
+                this.itemToMaxCount = itemToMaxCount;
+                maxNameLength = itemToMaxCount.Keys.Select(item => item.Name.Length).Max();
+                this.progressbarSettings = new ProgressbarSettings(
+                    progressBarEmpty: progressbarSettings.ProgressBarEmpty,
+                    progressbarFull: progressbarSettings.ProgressbarFull,
+                    progressBar100percent: progressbarSettings.ProgressBar100percent,
+                    lenght: display.Length - maxNameLength);
+            }
+
+            public void Render()
+            {
+                display.Clear();
+                if (headerText != null)
+                {
+                    display.PrintMiddle(headerText);
+                }
+
+                List<IMyInventory> inventories = Utils.GetAllInventory(containers);
+                List<Item> itemsToCalculate = itemToMaxCount.Keys.ToList();
+                Dictionary<Item, long> itemsToCount = Utils.CalculateItemsInAllInventory(inventories, itemsToCalculate);
+
+                itemsToCount.Keys.ToList().ForEach(item => display.Println(GetCargoInfoLine(item, itemsToCount[item])));
+            }
+
+            private String GetCargoInfoLine(Item item, long count)
+            {
+                int countSpaceAfterName = maxNameLength - item.Name.Length;
+                return item.Name + new string(' ', countSpaceAfterName) +
+                    Utils.GetProgressBar(count, itemToMaxCount[item], progressbarSettings);
+            }
+        }
+    }
+}
---
+++ SharedScripts/Scripts/CargoVolumeMonitor.cs
@@ -0,0 +1,73 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public class CargoVolumeMonitor : IMonitor
+        {
+
+            private readonly Display display;
+            private readonly Dictionary<List<IMyEntity>, string> groupContainersWithName;
+            private readonly int maxNameLength;
+            private readonly string headerText;
+            private readonly ProgressbarSettings progressbarSettings;
+
+            public CargoVolumeMonitor(Display display, Dictionary<List<IMyEntity>, string> groupContainersWithName, string headerText,
+                ProgressbarSettings progressbarSettings)
+            {
+                this.display = display;
+                this.groupContainersWithName = groupContainersWithName;
+                this.headerText = headerText;
+                maxNameLength = groupContainersWithName.Values.Select(name => name.Length).Max();
+                this.progressbarSettings = new ProgressbarSettings(
+                    progressBarEmpty: progressbarSettings.ProgressBarEmpty,
+                    progressbarFull: progressbarSettings.ProgressbarFull,
+                    progressBar100percent: progressbarSettings.ProgressBar100percent,
+                    lenght: display.Length - maxNameLength);
+            }
+
+            public void Render()
+            {
+                display.Clear();
+                if (headerText != null)
+                {
+                    display.PrintMiddle(headerText);
+                }
+
+                groupContainersWithName.Keys.ToList()
+                    .ForEach(containers => display.Println(GetVolumeInfoLine(containers, groupContainersWithName[containers])));
+            }
+
+            public string GetVolumeInfoLine(List<IMyEntity> containers, string name)
+            {
+                int countSpaceAfterName = maxNameLength - name.Length;
+
+                List<IMyInventory> inventories = Utils.GetAllInventory(containers);
+                long currentVolumeSum = inventories.Select(inventory => Utils.FromRaw(inventory.CurrentVolume.RawValue) * 1000).Sum();
+                long maxVolumeSum = inventories.Select(inventory => Utils.FromRaw(inventory.MaxVolume.RawValue) * 1000).Sum();
+
+                return name + new string(' ', countSpaceAfterName) +
+                    Utils.GetProgressBar(currentVolumeSum, maxVolumeSum, progressbarSettings);
+            }
+        }
+    }
+}
