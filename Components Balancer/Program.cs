57a396fdb85964351026b1185bda0de4013d2e47
Bronuh <bronyx@yandex.ru>
2023-02-07 20:21:34 +0300

    Закотовка для автокрафта

--- Components Balancer/Program.cs
+++ Components Balancer/Program.cs
@@ -26,19 +26,61 @@ namespace IngameScript
     /// </summary>
     partial class Program : MyGridProgram
     {
+        private int _tier = 1; // степень 10, на которую будет умножено требуемое количество предметов
+
+        // Список предметов для дозаказа
+        private readonly Dictionary<Item, long> _targetAmount = new Dictionary<Item, long>
+        {
+            { Items.Components.STEEL_PLATE, 1000 },
+            { Items.Components.INTEROR_PLATE, 1000},
+            { Items.Components.COMPUTER, 200 },
+            { Items.Components.CONSTRUCTION, 200 },
+            { Items.Components.SMALL_TUBE, 1500 },
+            { Items.Components.GIRDER, 750 },
+            { Items.Components.LARGE_TUBE, 100 },
+            { Items.Components.METAL_GRID, 300 },
+            { Items.Components.MOTOR, 750 },
+            { Items.Components.SOLAR_CELL, 50 },
+            { Items.Components.REACTOR, 100 },
+            { Items.Components.THRUST, 100 },
+            { Items.Components.POWER_CELL, 100 },
+            { Items.Components.MEDICAL, 3 },
+            { Items.Components.GRAVITY_GENERATOR, 20 },
+            { Items.Components.RADIO_COMMUNICATION, 20 },
+            { Items.Components.BULLETPROOF_GLASS, 100 },
+            { Items.Components.DETECTOR, 50 },
+            { Items.Components.DISPLAY, 150 },
+            { Items.Components.EXPLOSIVES, 100 }
+        };
+
         public Program()
         {
-            
+            Runtime.UpdateFrequency = UpdateFrequency.Update1;
+
+            Scheduler.ExecuteEveryNTicks(ScanGrid, 200);
+            Scheduler.ExecuteEveryNTicks(CheckStorage, 50);
+
+            Scheduler.Start();
         }
 
-        public void Save()
+        private void StartProducing(Item component, int amount)
         {
-            
+
+        }
+
+        private void ScanGrid()
+        {
+
+        }
+
+        private void CheckStorage()
+        {
+
         }
 
         public void Main(string argument, UpdateType updateSource)
         {
-            
+            Scheduler.Tick();
         }
     }
 }
--- Utils/Utils.cs
+++ Utils/Utils.cs
@@ -22,26 +22,22 @@ namespace IngameScript
 {
     partial class Program
     {
-		/// <summary>
-		/// Женя, заебал, напиши комментарии для тупых
-		/// </summary>
-		[Obsolete]
-        public static class Utils
-        {
+		public static class Utils
+		{
 
 			public static long FromRaw(long rawValue)
-            {
+			{
 				return rawValue / 1000000;
-            }
+			}
 
 			public static string GetShortNumber(long number, bool alignment)
 			{
-				char[] suffixes = {' ', 'K', 'M', 'G', 'T', 'P', 'E', '?'};
+				char[] suffixes = { ' ', 'K', 'M', 'G', 'T', 'P', 'E', '?' };
 				int numberLen = number.ToString().Length;
 
 				//number = numberBase * 10 ^ (3 * numberPower3)
 				int numberPower3 = (numberLen - 1) / 3;
-				long numberBase = number / (long) Math.Pow(10, numberPower3 * 3);
+				long numberBase = number / (long)Math.Pow(10, numberPower3 * 3);
 				int suffixIndex = Math.Min(numberPower3, suffixes.Length - 1);
 				char suffix = suffixes[suffixIndex];
 
@@ -50,7 +46,7 @@ namespace IngameScript
 			}
 
 			public static string GetHourTranslate(long hours)
-            {
+			{
 				long val1 = hours % 10;
 				long val10 = (hours % 100) / 10;
 
@@ -58,38 +54,24 @@ namespace IngameScript
 				{
 					return "час";
 				}
-				if (val10 != 1 && val1 >= 2 && val1 <= 4) {
+				if (val10 != 1 && val1 >= 2 && val1 <= 4)
+				{
 					return "часа";
 				}
 				return "часов";
 			}
 
-			public static string GetProgressBar(long count, long maxCount, ProgressbarSettings progressbarSettings)
-            {
-				const int COUNT_SPEC_CHARS = 13; // " [_____] 100K/999K" Number of chars without progress bar (symbols _____)
-
-				int length = Math.Max(progressbarSettings.Length - COUNT_SPEC_CHARS, 1);
-				double percent = maxCount != 0 ? (double) count / maxCount : 0;
-				int countCharFull = (int) Math.Floor(length * percent);
-				int countCharEmpty = length - countCharFull;
-
-				string progressbarChars = countCharEmpty > 0 ?
-					new string(progressbarSettings.ProgressbarFull, countCharFull) +
-						new string(progressbarSettings.ProgressBarEmpty, countCharEmpty) :
-					new string(progressbarSettings.ProgressBar100percent, length);
-				return $" [{progressbarChars}] {GetShortNumber(count, true)}/{GetShortNumber(maxCount, true)}";
-			}
 
 			public static List<IMyInventory> GetAllInventory(IMyEntity entity)
-            {
+			{
 				List<IMyInventory> inventories = new List<IMyInventory>(entity.InventoryCount);
-                for (int i = 0; i < entity.InventoryCount; i++)
-                {
+				for (int i = 0; i < entity.InventoryCount; i++)
+				{
 					inventories.Add(entity.GetInventory(i));
-                }
+				}
 
 				return inventories;
-            }
+			}
 
 			public static List<IMyInventory> GetAllInventory(List<IMyEntity> entities)
 			{
@@ -97,7 +79,7 @@ namespace IngameScript
 			}
 
 			public static Dictionary<Item, long> CalculateItemsInAllInventory(List<IMyInventory> inventories, List<Item> itemsToCalculate)
-            {
+			{
 				Dictionary<Item, long> itemToCount = itemsToCalculate.ToDictionary(item => item, item => 0L);
 				foreach (IMyInventory inventory in inventories)
 				{
@@ -107,16 +89,16 @@ namespace IngameScript
 			}
 
 			public static string GetGridName(IMyCubeBlock block)
-            {
+			{
 				return block.CubeGrid.CustomName;
-            }
+			}
 
 			public static Dictionary<string, List<T>> GetBlocksByGridName<T>(List<T> blocks) where T : IMyCubeBlock
-            {
+			{
 				return blocks
 					.GroupBy(block => GetGridName(block))
 					.ToDictionary(group => group.Key, group => group.ToList());
-            }
+			}
 		}
-    }
+	}
 }
